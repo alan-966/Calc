ee0fa16e28a4bc1c2050f6ab4ad1c0014f3003c3
Alan Gorbunov <alan.gorbunov@mail.ru>
2020-11-09 11:00:39 +0300

    Full commit

---
+++ components/Row.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+const Row = ({children}) => {
+	return(
+		<View style={{flexDirection: 'row'}}>
+			{children}
+		</View>
+	);
+};
+
+export default Row;
---
+++ ios/Pods/Target Support Files/Pods-MyCalc-MyCalcTests/Pods-MyCalc-MyCalcTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyCalc_MyCalcTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyCalc_MyCalcTests
+@end
